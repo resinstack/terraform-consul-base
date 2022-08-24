f5ae67475ca814daa626437a40d79dfc959056e2
Michael Aldridge <aldridge.mac@gmail.com>
2022-08-24 01:26:14 -0500

    Adjust vault policy w.r.t. deprecation of consul storage.

--- vault_policy.tf
+++ vault_policy.tf
@@ -4,30 +4,10 @@ resource "consul_acl_policy" "vault_server" {
   name        = "resin-vault-server"
   description = "Policy for Vault server"
   rules = jsonencode({
-    key_prefix = {
-      "vault/" = {
-        policy = "write"
-      }
-    },
-    node_prefix = {
-      "" = {
-        policy = "write"
-      }
-    },
     service = {
       vault = {
         policy = "write"
       }
     },
-    agent_prefix = {
-      "" = {
-        policy = "write"
-      }
-    },
-    session_prefix = {
-      "" = {
-        policy = "write"
-      }
-    }
   })
 }
